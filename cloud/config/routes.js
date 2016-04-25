ddd3e9c04494881563fc9889b8cdf3c1d82086c8
Mazi Muhlari <mazi.muhlari@gmail.com>
2016-04-25 20:34:51 +0200

    Almost done with home page

--- cloud/config/routes.js
+++ cloud/config/routes.js
@@ -6,6 +6,14 @@ module.exports = function(app) {
         res.sendfile('./web/views/index.html');
     });
 
+    app.get('/get-blessed', function(req, res) {
+        res.sendfile('./web/views/login.html');
+    });
+
+    app.get('/become-a-blesser', function(req, res) {
+        res.sendfile('./web/views/register.html');
+    });
+
     app.get('/messages', function(req, res) {
         res.sendfile('./web/views/messages.html');
     });
