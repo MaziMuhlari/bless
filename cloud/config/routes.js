dfe27303118c7620a1fbfbba8023f8781e3a45cf
Muhlari, Mkhomazi J <mkhomazi.j.muhlari@accenture.com>
2016-05-12 21:13:02 +0200

    Made send message email html

--- cloud/config/routes.js
+++ cloud/config/routes.js
@@ -278,8 +278,8 @@ module.exports = function(app, passport) {
 											sendgrid.send({
 													to:       recepient["username"],
 													from:     'blesserco@icloud.com',
-													subject:  'Blesser: New Message',
-													text:     sender["name"] + " " + sender["surname"] + 'said: ' + req.query.message + ' <a href="http://blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
+													subject:  'Blesser: Message from ' + sender["name"] + " " + sender["surname"],
+													html:     sender["name"] + " " + sender["surname"] + ': ' + req.query.message + ' <a href="http://blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
 											}, function(err, json) {
 													if (err) { 
 														return console.error(err); 
