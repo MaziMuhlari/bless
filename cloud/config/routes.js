7f63b49819bd934c8fb3912bef602f20291b24e4
Muhlari, Mkhomazi J <mkhomazi.j.muhlari@accenture.com>
2016-05-12 21:14:39 +0200

    Updated send message subject email

--- cloud/config/routes.js
+++ cloud/config/routes.js
@@ -278,7 +278,7 @@ module.exports = function(app, passport) {
 											sendgrid.send({
 													to:       recepient["username"],
 													from:     'blesserco@icloud.com',
-													subject:  'Blesser: Message from ' + sender["name"] + " " + sender["surname"],
+													subject:  'Message from ' + sender["name"] + " " + sender["surname"],
 													html:     sender["name"] + " " + sender["surname"] + ': ' + req.query.message + ' <a href="http://blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
 											}, function(err, json) {
 													if (err) { 
