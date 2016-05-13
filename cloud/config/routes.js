0abad7f3a28353541d5ce5daef4700cbf916745f
Muhlari, Mkhomazi J <mkhomazi.j.muhlari@accenture.com>
2016-05-13 09:32:24 +0200

    changed url for emails

--- cloud/config/routes.js
+++ cloud/config/routes.js
@@ -278,7 +278,7 @@ module.exports = function(app, passport) {
 													to:       recepient.username,
 													from:     'blesserco@icloud.com',
 													subject:  'Message from ' + sender.name + " " + sender.surname,
-													html:     sender.name + " " + sender.surname + ': ' + req.query.message + ' <a href="http://blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
+													html:     sender.name + " " + sender.surname + ': ' + req.query.message + ' <a href="http://www.blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
 											}, function(err, json) {
 													if (err) { 
 														return console.error(err); 
