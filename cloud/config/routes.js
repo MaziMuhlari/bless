2699bd84a158e90d82bef7522cde3622a5d13984
Mazi Muhlari <mazimuhlari@Mazis-MacBook-Pro.local>
2017-02-16 13:32:18 +0200

    Updates

--- cloud/config/routes.js
+++ cloud/config/routes.js
@@ -1,39 +1,39 @@
-var mongoose 				= require('mongoose');
-var User            = require('../models/user');
-var Message         = require('../models/message');
-var Conversation    = require('../models/conversation');
-var sendgrid       	= require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+var mongoose = require('mongoose');
+var User = require('../models/user');
+var Message = require('../models/message');
+var Conversation = require('../models/conversation');
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
-	if (req.isAuthenticated()){
+	if (req.isAuthenticated()) {
 		return next();
-  }
+	}
 	// if the user is not authenticated then redirect him to the login page
 	res.redirect('/login');
 }
 
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 
-  /**
-   * API
-   */
+	/**
+	 * API
+	 */
 
 	app.post('/register', passport.authenticate('register', {
 		successRedirect: '/',
 		failureRedirect: '/register',
-		failureFlash : true
+		failureFlash: true
 	}));
 
 	app.post('/login', passport.authenticate('login', {
 		successRedirect: '/',
 		failureRedirect: '/login',
-		failureFlash : true
+		failureFlash: true
 	}));
 
-	app.get('/log-out', function(req, res) {
+	app.get('/log-out', function (req, res) {
 		req.logout();
 		res.clearCookie('_id');
 		res.clearCookie('username');
@@ -49,124 +49,124 @@ module.exports = function(app, passport) {
 		res.redirect('/logged-out');
 	});
 
-  app.get('/api/user', isAuthenticated, function(req, res){
-    var user = {};
-    if (req.user) {
-      user = {
-        _id: req.user._id,
+	app.get('/api/user', isAuthenticated, function (req, res) {
+		var user = {};
+		if (req.user) {
+			user = {
+				_id: req.user._id,
 				username: req.user.username,
-		    gender: req.user.gender,
-		    is_active: req.user.is_active,
-		    name: req.user.name,
-		    surname: req.user.surname,
-		    description: req.user.description,
-		    is_blesser: req.user.is_blesser,
-		    blessing: req.user.blessing,
-		    created_on: req.user.created_on,
+				gender: req.user.gender,
+				is_active: req.user.is_active,
+				name: req.user.name,
+				surname: req.user.surname,
+				description: req.user.description,
+				is_blesser: req.user.is_blesser,
+				blessing: req.user.blessing,
+				created_on: req.user.created_on,
 				logged_in: true
-      };
-    } else {
-      user = {
-        logged_in: false
-      };
-    }
-    res.json(user);
-  });
+			};
+		} else {
+			user = {
+				logged_in: false
+			};
+		}
+		res.json(user);
+	});
 
-	app.post('/api/user/:id', isAuthenticated, function(req, res) {
-    User.findOne({
-        _id: req.params.id
-    }, function(err, data){
-      data.name = req.body.name;
-	    data.gender = req.body.gender;
-      data.surname = req.body.surname;
-      data.description = req.body.description;
+	app.post('/api/user/:id', isAuthenticated, function (req, res) {
+		User.findOne({
+			_id: req.params.id
+		}, function (err, data) {
+			data.name = req.body.name;
+			data.gender = req.body.gender;
+			data.surname = req.body.surname;
+			data.description = req.body.description;
 			data.is_blesser = req.body.is_blesser;
-      data.blessing = req.body.blessing;
-      data.save(function (err){
-          if(err){
-              res.send("Error: " + err);
-          }else{
-              res.json(data);
-          }
-      })
-    });
-  });
-
-	app.get('/api/user/blessers', function(req, res) {
-    User
-		.find({ is_blesser: true }, { password: 0, username: 0 })
-		.sort({created_on: 'asc'})
-		.limit(req.query.take)
-		.skip(req.query.skip)
-		.exec(function(err, data){
-			if(err){
-					res.json(err);
-			}else{
+			data.blessing = req.body.blessing;
+			data.save(function (err) {
+				if (err) {
+					res.send("Error: " + err);
+				} else {
 					res.json(data);
-			}
+				}
+			})
 		});
-  });
+	});
+
+	app.get('/api/user/blessers', function (req, res) {
+		User
+			.find({ is_blesser: true }, { password: 0, username: 0 })
+			.sort({ created_on: 'asc' })
+			.limit(parseInt(req.query.take))
+			.skip(parseInt(req.query.skip))
+			.exec(function (err, data) {
+				if (err) {
+					res.json(err);
+				} else {
+					res.json(data);
+				}
+			});
+	});
 
 	// security
 
-	app.get('/api/security/loggin-in', function(req, res) {
+	app.get('/api/security/loggin-in', function (req, res) {
 		if (req.user) {
 			res.json(true);
 		} else {
 			res.json(false);
 		}
-  });
+	});
 
 	// Conversation
 
-	app.post('/api/conversation', isAuthenticated, function(req, res){
+	app.post('/api/conversation', isAuthenticated, function (req, res) {
 		Conversation.findOne({
 			$and: [
 				{ 'recepients': mongoose.Types.ObjectId(req.query.creator) },
 				{ 'recepients': mongoose.Types.ObjectId(req.query.recepient) }
 			]
 		})
-		.populate("recepients")
-		.exec(function(err, conversation){
-		    if (err){
-	        res.send(err);
-				} else if(conversation){
+			.populate("recepients")
+			.exec(function (err, conversation) {
+				if (err) {
+					res.send(err);
+				} else if (conversation) {
 					var arr = [];
 					arr.push(mongoose.Types.ObjectId(req.query.creator));
 					Array.prototype.push.apply(conversation.read, arr);
-          conversation.save(function (err){
-              if(err){
-                  res.send(err);
-              }else{
-								for (var x = 0; x < conversation.recepients.length; x++) {
-									conversation.recepients[x].username = "";
-									conversation.recepients[x].password = "";
-								}
-                res.json(conversation);
-              }
-          });
-				}else{
+					conversation.save(function (err) {
+						if (err) {
+							res.send(err);
+						} else {
+							for (var x = 0; x < conversation.recepients.length; x++) {
+								conversation.recepients[x].username = "";
+								conversation.recepients[x].password = "";
+							}
+							res.json(conversation);
+						}
+					});
+				} else {
 					var recepients = [];
 					recepients.push(req.query.creator);
 					recepients.push(req.query.recepient);
 					Conversation.create({
 						creator: req.query.creator,
 						recepients: recepients
-					}, function(err, conversation){
-					    if (err){
-				        res.send(err);
-							} else {
-								Conversation.findOne({
-									$and: [
-										{ 'recepients': mongoose.Types.ObjectId(req.query.creator) },
-										{ 'recepients': mongoose.Types.ObjectId(req.query.recepient) }
-									]
-								})
+					}, function (err, conversation) {
+						if (err) {
+							res.send(err);
+						} else {
+							Conversation.findOne({
+								$and: [
+									{ 'recepients': mongoose.Types.ObjectId(req.query.creator) },
+									{ 'recepients': mongoose.Types.ObjectId(req.query.recepient) }
+								]
+							})
 								.populate("recepients")
-								.exec(function(err, conversation){
-									if (err){
-						        res.send(err);
+								.exec(function (err, conversation) {
+									if (err) {
+										res.send(err);
 									} else {
 										for (var x = 0; x < conversation.recepients.length; x++) {
 											conversation.recepients[x].username = "";
@@ -175,21 +175,21 @@ module.exports = function(app, passport) {
 										res.json(conversation);
 									}
 								});
-							}
+						}
 					});
 				}
-		});
-  });
+			});
+	});
 
-	app.get('/api/conversations', isAuthenticated, function(req, res){
+	app.get('/api/conversations', isAuthenticated, function (req, res) {
 		Conversation.find({
-			 recepients: mongoose.Types.ObjectId(req.query.user_id)
+			recepients: mongoose.Types.ObjectId(req.query.user_id)
 		})
-		.populate("recepients")
-		.sort({last_message_sent: 'desc'})
-		.exec(function(err, conversations){
-		    if (err){
-	        res.send(err);
+			.populate("recepients")
+			.sort({ last_message_sent: 'desc' })
+			.exec(function (err, conversations) {
+				if (err) {
+					res.send(err);
 				} else {
 					for (var i = 0; i < conversations.length; i++) {
 						for (var x = 0; x < conversations[i].recepients.length; x++) {
@@ -199,109 +199,109 @@ module.exports = function(app, passport) {
 					}
 					res.json(conversations);
 				}
-		});
-  });
+			});
+	});
 
-	app.get('/api/conversations/unread', isAuthenticated, function(req, res){
+	app.get('/api/conversations/unread', isAuthenticated, function (req, res) {
 		res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-	  res.header('Expires', '-1');
-	  res.header('Pragma', 'no-cache');
+		res.header('Expires', '-1');
+		res.header('Pragma', 'no-cache');
 		Conversation.count({
-			 recepients: mongoose.Types.ObjectId(req.query.user_id),
-			 read: { '$ne': mongoose.Types.ObjectId(req.query.user_id) }
-		}, function(err, conversations){
-		    if (err){
-	        res.send(err);
-				} else {
-					res.json(conversations);
-				}
+			recepients: mongoose.Types.ObjectId(req.query.user_id),
+			read: { '$ne': mongoose.Types.ObjectId(req.query.user_id) }
+		}, function (err, conversations) {
+			if (err) {
+				res.send(err);
+			} else {
+				res.json(conversations);
+			}
 		});
-  });
+	});
 
 	// Message
 
-	app.get('/api/messages', isAuthenticated, function(req, res){
+	app.get('/api/messages', isAuthenticated, function (req, res) {
 		Message
-		.find({ conversation: req.query.conversation_id })
-		.sort({created_on: 'asc'})
-		.exec(function(err, messages){
-		    if (err){
-	        res.send(err);
+			.find({ conversation: req.query.conversation_id })
+			.sort({ created_on: 'asc' })
+			.exec(function (err, messages) {
+				if (err) {
+					res.send(err);
 				} else {
 					res.json(messages);
 				}
-		});
-  });
+			});
+	});
 
 
-	app.post('/api/message/send', isAuthenticated, function(req, res){
+	app.post('/api/message/send', isAuthenticated, function (req, res) {
 		Message.create({
 			message: req.query.message,
 			conversation: req.query.conversation_id,
 			composer: req.query.composer,
-		}, function(err, message){
-		    if (err){
-	        res.send(err);
-				} else {
-					var arr = [];
-					arr.push(mongoose.Types.ObjectId(req.query.composer));
-					Conversation.update({
-						_id: req.query.conversation_id
-					}, {
-						$set : {
+		}, function (err, message) {
+			if (err) {
+				res.send(err);
+			} else {
+				var arr = [];
+				arr.push(mongoose.Types.ObjectId(req.query.composer));
+				Conversation.update({
+					_id: req.query.conversation_id
+				}, {
+						$set: {
 							last_message_excerpt: req.query.message,
 							read: arr,
-					    last_message_sent: new Date()
+							last_message_sent: new Date()
 						}
-					}).exec(function(err, conversation){
-						if (err){
-				        res.send(err);
-							} else {
-								Conversation.findOne({
-									_id: req.query.conversation_id
-								})
+					}).exec(function (err, conversation) {
+						if (err) {
+							res.send(err);
+						} else {
+							Conversation.findOne({
+								_id: req.query.conversation_id
+							})
 								.populate("recepients")
-								.exec(function(err, conversation){
-										if (err){
-											res.send(err);
-										} else {
-											var sender;
-											var recepient;
-											for (var i = 0; i < conversation.recepients.length; i++) {
-												if(conversation.recepients[i]["_id"] == req.query.composer){
-													sender = conversation.recepients[i];
-												}else{
-													recepient = conversation.recepients[i];
-												}
+								.exec(function (err, conversation) {
+									if (err) {
+										res.send(err);
+									} else {
+										var sender;
+										var recepient;
+										for (var i = 0; i < conversation.recepients.length; i++) {
+											if (conversation.recepients[i]["_id"] == req.query.composer) {
+												sender = conversation.recepients[i];
+											} else {
+												recepient = conversation.recepients[i];
 											}
-											sendgrid.send({
-													to:       recepient.username,
-													from:     'blesserco@icloud.com',
-													subject:  'Message from ' + sender.name + " " + sender.surname,
-													html:     sender.name + " " + sender.surname + ': ' + req.query.message + ' <a href="http://www.blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
-											}, function(err, json) {
-													if (err) { 
-														return console.error(err); 
-													} else {
-														console.log(json);
-													}
-											});
 										}
+										sendgrid.send({
+											to: recepient.username,
+											from: 'blesserco@icloud.com',
+											subject: 'Message from ' + sender.name + " " + sender.surname,
+											html: sender.name + " " + sender.surname + ': ' + req.query.message + ' <a href="http://www.blesser.co/messages/' + req.query.conversation_id + '">reply here</a>'
+										}, function (err, json) {
+											if (err) {
+												return console.error(err);
+											} else {
+												console.log(json);
+											}
+										});
+									}
 								});
-							}
+						}
 					});
 
-					res.json(message);
-					
-				}
+				res.json(message);
+
+			}
 		});
-  });
+	});
 
-  /**
-   * VIEWS
-   */
+	/**
+	 * VIEWS
+	 */
 
-  app.get('/', function(req, res) {
+	app.get('/', function (req, res) {
 		if (req.user) {
 			res.cookie('_id', req.user._id, { maxAge: 2592000000 });
 			res.cookie('username', req.user.username, { maxAge: 2592000000 });
@@ -314,7 +314,7 @@ module.exports = function(app, passport) {
 			res.cookie('blessing', req.user.blessing, { maxAge: 2592000000 });
 			res.cookie('created_on', req.user.created_on, { maxAge: 2592000000 });
 			res.cookie('logged_in', true, { maxAge: 2592000000 });
-    } else {
+		} else {
 			res.clearCookie('_id');
 			res.clearCookie('username');
 			res.clearCookie('gender');
@@ -326,43 +326,43 @@ module.exports = function(app, passport) {
 			res.clearCookie('blessing');
 			res.clearCookie('created_on');
 			res.clearCookie('logged_in');
-    }
-    res.sendfile('./web/views/index.html');
-  });
+		}
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/about', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/about', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/privacy', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/privacy', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/terms', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/terms', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/cookies', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/cookies', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/contact', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/contact', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/login', function(req, res) {
-      res.render('login', { messages: req.flash('message') });
-  });
+	app.get('/login', function (req, res) {
+		res.render('login', { messages: req.flash('message') });
+	});
 
-  app.get('/register', function(req, res) {
-      res.render('register', { messages: req.flash('message') });
-  });
+	app.get('/register', function (req, res) {
+		res.render('register', { messages: req.flash('message') });
+	});
 
-  app.get('/report', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/report', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/logged-out', function(req, res) {
+	app.get('/logged-out', function (req, res) {
 		res.clearCookie('_id');
 		res.clearCookie('username');
 		res.clearCookie('gender');
@@ -374,29 +374,29 @@ module.exports = function(app, passport) {
 		res.clearCookie('blessing');
 		res.clearCookie('created_on');
 		res.clearCookie('logged_in');
-    res.sendfile('./web/views/index.html');
-  });
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/messages', isAuthenticated, function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/messages', isAuthenticated, function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-	app.get('/messages/:id', isAuthenticated, function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/messages/:id', isAuthenticated, function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/profile', isAuthenticated, function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/profile', isAuthenticated, function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('/page-not-found', function(req, res) {
-      res.sendfile('./web/views/index.html');
-  });
+	app.get('/page-not-found', function (req, res) {
+		res.sendfile('./web/views/index.html');
+	});
 
-  app.get('*', function(req, res) {
-    if(res.status(404)){
-      res.redirect('/page-not-found');
-    }
-  });
+	app.get('*', function (req, res) {
+		if (res.status(404)) {
+			res.redirect('/page-not-found');
+		}
+	});
 
 };
